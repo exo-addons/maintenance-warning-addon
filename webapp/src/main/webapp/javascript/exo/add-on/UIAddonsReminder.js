e3885568f56dbfebf2f42a3670eba3bbeaa97783
An Bao NGUYEN <annb@exoplatform.com>
2015-01-13 08:51:28 +0700

    PLFENG-2904: Improvements

--- webapp/src/main/webapp/javascript/exo/add-on/UIAddonsReminder.js
+++ webapp/src/main/webapp/javascript/exo/add-on/UIAddonsReminder.js
@@ -23,6 +23,8 @@ reminderObj.init = function(){
     var remainingMinutes; 
 
 
+
+
     //close popup then repeat after timer interval 
     gj(".uiIconClose").click(function() {
     gj("#blockPopUp").hide();
@@ -35,8 +37,13 @@ reminderObj.init = function(){
        gj("#blockPopUp").hide();
        clearInterval(timerReminder);
        timerReminder = setInterval(FetchData, INTERVAL_DISPLAY_POPUP * 1000);
+       var valueInput= "reminder--"+gj("#reminderContent input" ).val();  
        if (gj('input#reminderDismiss').is(':checked')) {
-        isPopUpActivate = false;
+        isPopUpActivate = true;
+        gj.cookie(valueInput, false);
+        }
+        else{
+        isPopUpActivate = true;
         }
     });
 
@@ -56,38 +63,67 @@ reminderObj.init = function(){
                   if (element.description)
                     {
                       //update INTERVAL
-                      INTERVAL_DISPLAY_POPUP = element.repeatIntervalMinute * 60;
+                      //INTERVAL_DISPLAY_POPUP = element.repeatIntervalMinute * 60;
                       var d = new Date();
                       var currentTime = d.getTime(); 
+                      var inputHidden ="<input type='hidden' value="+element.fromDate.time+" id="+element.fromDate.time+" />";
                       remainingMinutes = Math.round((element.fromDate.time - currentTime)/(60*1000));
                       var reminderDescription = splitData(element.description );
-                      gj("#reminderContent").html("" + htmlForTextWithEmbeddedNewlines(reminderDescription, remainingMinutes ));
                       isDescriptionExist = true;
+                      storeCookies(reminderDescription, element.fromDate.time ,remainingMinutes, isDescriptionExist, inputHidden );
                     }
                   }
                 );
 		       	}
 		      });
 
-          //if checkbox and description exist
-          if(remainingMinutes > 0){
-          if (!gj('input#reminderDismiss').is(':checked') && isDescriptionExist) {gj("#blockPopUp").show();} 
-          }
-          else{
-           gj("#blockPopUp").hide();
-          }
-             
+
+
+
         }
       }
+
+
+
     timerReminder = setInterval(FetchData, INTERVAL_DISPLAY_POPUP * 1000);
 
   //split data JSON, get only Description, add remaining time minute
-  function splitData(data){
-    var res1 = data.split("Description:");
-    var res = res1[1].split("<br>")[0];
-  return res;
+  function splitData(data)
+    {
+      var res1 = data.split("Description:");
+      var res = res1[1].split("<br>")[0];
+    return res;
+    }
   }
-  }
+
+
+    //search on cookie, if reminder--time is true, display the popup
+    function storeCookies(reminderDescription, time, remainingMinutes, isDescriptionExist, inputHidden )
+      {
+        //if cookie is true, display the popup with correct description, otherwise don't
+        if (typeof gj.cookie("reminder--"+time) === 'undefined') {
+                gj.cookie("reminder--"+time, true);
+        }  
+        else{
+
+                if (gj.cookie("reminder--"+time) == 'true'){
+                console.log("DISPLAY popupW "+"reminder--"+time + ">>"+ gj.cookie("reminder--"+time) );
+
+                if(remainingMinutes > 0 && isDescriptionExist )
+                    {
+                     console.log(remainingMinutes +"---"+ !gj('input#reminderDismiss').is(':checked') +"--"+ isDescriptionExist );
+                     gj("#reminderContent").html(""+htmlForTextWithEmbeddedNewlines(reminderDescription,remainingMinutes) + inputHidden);
+                     gj('input#reminderDismiss').prop('checked', false);
+                     gj("#blockPopUp").show();
+                    }
+                }
+                else{
+                    console.log(remainingMinutes +"---"+ !gj('input#reminderDismiss').is(':checked') +"--"+ isDescriptionExist );
+                    console.log("DON'T DISPLAYTRUW "+"reminder--"+time +">>"+ gj.cookie("reminder--"+time) );
+                }           
+        }
+
+      }
 
 function htmlForTextWithEmbeddedNewlines(text, remainingMinutes) {
     var htmls = [];
@@ -96,9 +132,7 @@ function htmlForTextWithEmbeddedNewlines(text, remainingMinutes) {
     for (var i = 0 ; i < lines.length ; i++) {
         var text = tmpDiv.text(lines[i]).html();
       if (text.indexOf("%remaining_time%")){
-        text = text.replace("%remaining_time%", "<p class='center'><span class='minutes'>"+remainingMinutes+"</span> minutes</p>"); 
-
-
+        text = text.replace("%remaining_time%", "<p style='margin-right: 5em; margin-top: 1em;' class='center'><span class='minutes'>"+remainingMinutes+"</span> minutes</p>"); 
       }
         htmls.push(text);
     }
